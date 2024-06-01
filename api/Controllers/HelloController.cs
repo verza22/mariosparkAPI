d650556ef664e552e1eb593212131c678b9d6fe7
Luis Zurita <zuritabal@gmail.com>
2024-06-01 08:09:52 -0500

    primer commit

---
+++ api/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        // GET: HelloController
+        [HttpGet]
+        public string Get()
+        {
+            return "test";
+        }
+
+        [HttpGet("test")]
+        public string test()
+        {
+            return "test 2000";
+        }
+    }
+}
