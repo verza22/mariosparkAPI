a8a2af338e5804fd80fb504418d0c0f7a8c0c29b
Luis Zurita <zuritabal@gmail.com>
2024-07-11 19:37:12 -0500

    widgets1

---
+++ api/Models/Enumeration.cs
@@ -0,0 +1,31 @@
+﻿namespace api.Models
+{
+    public enum WidgetType
+    {
+        Kpi = 0,
+        List = 1,
+        Pie = 2,
+        Column = 3
+    }
+
+    public enum WidgetInfoType
+    {
+        OrderTotal = 0,
+        OrderCount = 1,
+        HotelOrderTotal = 2,
+        HotelOrderCount = 3,
+        WaiterOrderTotal = 4,
+        WaiterOrderCount = 5,
+        CustomerCount = 6,
+        ProductCount = 7,
+        CategoryCount = 8,
+        OtherCustomerCount = 9,
+        OtherProductCount = 10
+    }
+
+    public enum DateType
+    {
+        Fixed = 0,
+        Dynamic = 1
+    }
+}
---
+++ api/Models/Widget.cs
@@ -0,0 +1,25 @@
+﻿namespace api.Models
+{
+    public class Widget
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+        public string Title { get; set; }
+        public string Symbol { get; set; }
+        public bool IsLeading { get; set; }
+        public WidgetInfoType InfoType { get; set; }
+        public WidgetType Type { get; set; }
+
+        //dates
+        public string DateFrom { get; set; }
+        public DateType DateFromType { get; set; }
+        public string DateTo { get; set; }
+        public DateType DateToType { get; set; }
+
+        //general config
+        public int Position { get; set; }
+        public int SizeX { get; set; }
+        public int SizeY { get; set; }
+        public string BgColor { get; set; }
+    }
+}
